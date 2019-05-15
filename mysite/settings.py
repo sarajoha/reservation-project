27ebd3d5e92213d1fde0b230186c27a449e869d7
sarajoha <sjcc333@gmail.com>
2019-05-15 11:36:45 -0500

    Added celery beat and a task

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'bootstrap_datepicker_plus',
     'django_extensions',
     'django.contrib.humanize',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
--- reservation/tasks.py
+++ reservation/tasks.py
@@ -1,6 +1,8 @@
 #Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from .models import Reservation, User
+from django.utils import timezone
 
 
 @shared_task
@@ -16,3 +18,12 @@ def mul(x, y):
 @shared_task
 def xsum(numbers):
     return sum(numbers)
+
+#periodic task to be run every monday morning with celery beats
+def user_with_most_reservations():
+    last_week = timezone.now() - timezone.timedelta(weeks=1)
+    yesterday = timezone.now() - timezone.timedelta(days=1)
+
+    #user that made more reservations last week
+    user = User.objects.distinct().filter(reservation__start_datetime__range=(last_week, yesterday)).annotate(count=Count('reservation')).order_by('-count').first()
+    return user.get_full_name()
